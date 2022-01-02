d108373c425dbc3db47a031fd9c56fcac8e90f17
Nana Pisanko <bonjozzy@gmail.com>
2022-01-02 20:40:42 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let arr = n.toString().split('').reverse();
   
+    if (arr[0] == '0') {
+      arr = arr.slice(1);
+    }
+    
+    if (arr[arr.length - 1] == '-') {
+      arr = arr.slice(0, - 1);
+    }
+    return arr.join('');
 }
